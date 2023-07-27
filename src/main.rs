1811f6cbe3dcf83c4d1eda2bfd3343e3b105dd69
JustCris654 <cristian.scapin654@icloud.com>
2023-07-27 21:32:20 +0200

    refactor: removed thread and command queue

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,6 @@ fn main() {
 
     // thread::sleep(Duration::from_secs(5));
 
-    let events_queue = Arc::new(Mutex::new(Vec::new()));
     let (sender, receiver) = mpsc::channel();
     // let receiver = Arc::new(Mutex::new(receiver));
 
@@ -45,42 +44,36 @@ fn main() {
     });
 
     println!("Listening for events");
-    let events_queue_recv = Arc::clone(&events_queue);
     let receiver = thread::spawn(move || {
         for event in receiver.iter() {
             println!("Event: {:?}", event);
             if let Some(input) = event.name {
                 let input = input.to_lowercase();
-                events_queue_recv.lock().unwrap().push(input);
-            }
-        }
-    });
 
-    sink.set_volume(0.5);
-
-    println!("Handling events");
-    let events_queue_send = Arc::clone(&events_queue);
-    let handler = thread::spawn(move || loop {
-        if let Some(event) = events_queue_send.lock().unwrap().pop() {
-            match event.as_str() {
-                " " => {
-                    if sink.is_paused() {
-                        sink.play();
-                    } else {
-                        sink.pause();
+                match input.as_str() {
+                    " " => {
+                        if sink.is_paused() {
+                            sink.play();
+                        } else {
+                            sink.pause();
+                        }
                     }
+                    "q" => {
+                        thread::sleep(std::time::Duration::from_millis(50));
+                        sink.stop();
+                        return;
+                    }
+                    "up" => {
+                        sink.set_volume(sink.volume() + 0.1);
+                    }
+                    "down" => {
+                        sink.set_volume(sink.volume() - 0.1);
+                    }
+                    _ => {}
                 }
-                "up" => {
-                    sink.set_volume(sink.volume() + 0.1);
-                }
-                "down" => {
-                    sink.set_volume(sink.volume() - 0.1);
-                }
-                _ => {}
             }
         }
     });
 
-    handler.join().unwrap();
     receiver.join().unwrap();
 }
