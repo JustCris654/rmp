a240c378bb76da8d2c53d7fcd245728213e1049c
JustCris654 <cristian.scapin654@icloud.com>
2023-10-25 08:34:07 +0200

    communication from input thread to player thread with channels

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use rodio::OutputStream;
 use std::fs::metadata;
 use std::io::{stdout, Write};
 use std::path::Path;
-use std::sync::Arc;
+use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread;
 
 #[derive(Parser)]
@@ -48,6 +48,23 @@ fn print_volume(rmp: &RMPlayer) {
     stdout.flush().unwrap();
 }
 
+enum ChannelCommands {
+    PlayPause,
+    Next,
+    Previous,
+    VolumeUp,
+    VolumeDown,
+    Forward,
+    Backward,
+    Shuffle,
+}
+
+fn check_fill_sink(rmp: &RMPlayer) {
+    if rmp.get_sink_len() <= 1 && rmp.get_infinte() {
+        rmp.fill_sink();
+    }
+}
+
 fn main() {
     let args = Args::parse();
     let filepath = args.path.unwrap();
@@ -57,12 +74,13 @@ fn main() {
     let md = metadata(filepath).unwrap();
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let rmplayer = Arc::new(RMPlayer::new(
+    let rmplayer = RMPlayer::new(
         stream_handle.clone(),
         filepath.to_str().unwrap().to_string(),
         args.shuffle,
         args.infinite,
-    ));
+    );
+    let (tx, rx): (Sender<ChannelCommands>, Receiver<ChannelCommands>) = mpsc::channel();
 
     assert!(
         !md.is_file(),
@@ -82,15 +100,31 @@ fn main() {
     // checks every 1 second if the sink has less than one file in the queue, if true
     // and infinite flag is set reappend the queue of files in the sink
     // if false do nothing
-    let infinite = rmplayer.get_infinte();
     let _sink_handler = thread::spawn(move || loop {
-        thread::sleep(std::time::Duration::from_secs(1));
+        thread::sleep(std::time::Duration::from_millis(100));
 
+        // check for user input
+        if let Ok(input) = rx.try_recv() {
+            match input {
+                ChannelCommands::PlayPause => rmplayer.play_pause(),
+                ChannelCommands::Next => {
+                    check_fill_sink(&rmplayer);
+                    rmplayer.next();
+                }
+                ChannelCommands::Previous => panic!("TODO: go to previous track"),
+                ChannelCommands::VolumeUp => rmplayer.volume_up(),
+                ChannelCommands::VolumeDown => rmplayer.volume_down(),
+                ChannelCommands::Forward => panic!("TODO: forward 5 seconds"),
+                ChannelCommands::Backward => panic!("TODO: backward 5 seconds"),
+                ChannelCommands::Shuffle => panic!("TODO: shuffle"),
+            }
+        }
+
+        // print current song title on the terminal
         print_current_song(&rmplayer);
 
-        if rmplayer.get_sink_len() <= 1 && infinite {
-            rmplayer.fill_sink();
-        }
+        // append tracks to the sink again if only one remain
+        check_fill_sink(&rmplayer);
     });
 
     let input_handler = thread::spawn(move || {
@@ -98,19 +132,35 @@ fn main() {
             if let Event::Key(event) = read().unwrap() {
                 match event.code {
                     KeyCode::Esc | KeyCode::Char('q') => break,
-                    KeyCode::Char(' ') => {}
+                    KeyCode::Char(' ') => {
+                        tx.send(ChannelCommands::PlayPause).unwrap();
+                    }
                     KeyCode::Char('s') => {
                         // shuffle
+                        tx.send(ChannelCommands::Shuffle).unwrap();
                     }
                     KeyCode::Char('n') | KeyCode::Char('l') => {
                         // next music in track list
+                        tx.send(ChannelCommands::Next).unwrap();
                     }
                     KeyCode::Char('p') | KeyCode::Char('h') => {
                         // previous in track list
+                        tx.send(ChannelCommands::Previous).unwrap();
                     }
-                    KeyCode::Char('k') | KeyCode::Up => {}
+                    KeyCode::Char('k') | KeyCode::Up => {
+                        tx.send(ChannelCommands::VolumeUp).unwrap();
+                    }
                     KeyCode::Char('j') | KeyCode::Down => {
                         // volume down
+                        tx.send(ChannelCommands::VolumeDown).unwrap();
+                    }
+                    KeyCode::Right => {
+                        // volume go forward
+                        tx.send(ChannelCommands::Forward).unwrap();
+                    }
+                    KeyCode::Left => {
+                        // volume go backward
+                        tx.send(ChannelCommands::Backward).unwrap();
                     }
                     _ => {}
                 }
